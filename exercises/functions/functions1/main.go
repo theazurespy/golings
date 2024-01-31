e74f63f51102a5f4c68195139f955c93a3220905
Bryce Murphy <98229344+theazurespy@users.noreply.github.com>
2024-01-31 14:25:00 +0000

    1st set done

--- exercises/functions/functions1/main.go
+++ exercises/functions/functions1/main.go
@@ -1,8 +1,9 @@
 // functions1
 // Make me compile!
 
-// I AM NOT DONE
 package main
+
+func call_me() {}
 
 func main() {
 	call_me()
--- exercises/switch/switch2/main.go
+++ exercises/switch/switch2/main.go
@@ -1,7 +1,5 @@
 // switch2
 // Make me compile!
-
-// I AM NOT DONE
 package main
 
 import "fmt"
@@ -10,7 +8,7 @@ func main() {
 	switch {
 	case 0 > 1:
 		fmt.Println("zero is greater than one")
-	case:
+	case 0 < 1:
 		fmt.Println("one is greater than zero")
 	}
 }
